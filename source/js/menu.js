db7fa2863dc7658a335038ea9149787f9530c7f4
Settimo <prytkov.n@yandex.ru>
2018-10-09 14:45:10 +0400

    Реализовал выпадающее меню на мобильной версии

---
+++ source/js/menu.js
@@ -0,0 +1,16 @@
+var mainNavigation = document.querySelector('.main-navigation');
+var navigationToggle = mainNavigation.querySelector('.main-navigation__toggle');
+
+console.log(mainNavigation);
+
+mainNavigation.classList.remove('main-navigation--nojs');
+
+navigationToggle.addEventListener('click', function() {
+  if (mainNavigation.classList.contains('main-navigation--closed')) {
+    mainNavigation.classList.remove('main-navigation--closed');
+    mainNavigation.classList.add('main-navigation--opened');
+  } else {
+    mainNavigation.classList.remove('main-navigation--opened');
+    mainNavigation.classList.add('main-navigation--closed');
+  }
+});
